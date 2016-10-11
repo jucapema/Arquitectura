aaddf49efa319603035adb591526c607d81ab188
jucapema <camilo89@utp.edu.co>
2016-10-11 18:40:03 -0500

    Add files via upload

---
+++ tb_Procesador.vhd
@@ -0,0 +1,66 @@
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY tb_Procesador IS
+END tb_Procesador;
+ 
+ARCHITECTURE behavior OF tb_Procesador IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Procesador
+    PORT(
+         clk : IN  std_logic;
+         reset : IN  std_logic;
+         Alu : OUT  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal reset : std_logic := '0';
+
+ 	--Outputs
+   signal Alu : std_logic_vector(31 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 20 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Procesador PORT MAP (
+          clk => clk,
+          reset => reset,
+          Alu => Alu
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      reset <= '1';
+		wait for 20 ns;	
+				reset <= '0'; 
+
+
+      wait;
+   end process;
+
+END;
